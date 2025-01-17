d296428ab9c2952ca4da62ef81fd1129c72ff83c
WebDevCF2m <gitweb@cf2m.be>
2025-01-17 15:02:41 +0100

    first commit

---
+++ datas/datas.php
@@ -0,0 +1,46 @@
+<?php
+$datas = [
+    "titre" => "First site",
+    "pages" => [
+        "accueil" => [
+            "titrepage"=> "Homepage",
+            "text" => "On écrit « acceuil » ou « accueil » ?
+Règle : On écrit toujours « accueil » avec le -u après le double -c. La définition de ce mot désigne « le fait d’accueillir, la manière d’accueillir ».
+
+Explications : dans la plupart des cas, le son [oeil] qu'on écrit « oeil » s’écrit -euil en fin de mot, comme par exemple dans « fauteuil », « deuil », « écureuil » etc. Il y a toutefois des exceptions à cette règle, les noms accueil, cercueil, écueil, orgueil et recueil s'écrivent -ueil. Ces exceptions sont le résultat d'un constat simple : l'écriture de ceuil et geuil se prononcerait respectivement [sœj] et [Gœj]. Ainsi, on inverse le -u et le -e pour conserver les prononciations [k] et [g]. En résumé, on écrit -ueil dans deux cas :
+
+lorsque le son [œj] est précédé de la lettre -c, on écrira -ueil pour conserver le son [k]
+lorsque le son [œj] est précédé de la lettre -g, on écrira -ueil pour conserver le son [g]"
+        ],
+        "php" => [
+            "titrepage"=> "PHP",
+            "text" => "PHP: Hypertext Preprocessor6, plus connu sous son sigle PHP (sigle auto-référentiel), est un langage de programmation libre7, principalement utilisé pour produire des pages Web dynamiques via un serveur web6, mais pouvant également fonctionner comme n'importe quel langage interprété de façon locale. PHP est un langage impératif orienté objet.
+
+PHP a permis de créer un grand nombre de sites web célèbres, comme Facebook et Wikipédia8. Il est considéré comme une des bases de la création de sites web dits dynamiques mais également des applications web."
+        ],
+        "sql" => [
+            "titrepage"=> "SQL",
+            "text" => "SQL (sigle pour Structured Query Language, « langage de requêtes structurées ») est un langage informatique normalisé servant à exploiter des bases de données relationnelles. La partie langage de manipulation des données de SQL permet de rechercher, d'ajouter, de modifier ou de supprimer des données dans les bases de données relationnelles.
+
+Outre le langage de manipulation des données :
+
+le langage de définition des données permet de créer et de modifier l'organisation des données dans la base de données,
+le langage de contrôle de transaction permet de commencer et de terminer des transactions,
+le langage de contrôle des données permet d'autoriser ou d'interdire l'accès à certaines données (base, table, enregistrement) à certaines personnes."
+        ],
+        "javascript" => [
+            "titrepage"=> "JavaScript",
+            "text" => "JavaScript est un langage de programmation de scripts principalement employé dans les pages web interactives et à ce titre est une partie essentielle des applications web. Avec les langages HTML et CSS, JavaScript est au cœur des langages utilisés par les développeurs web4. Une grande majorité des sites web l'utilisent5, et la majorité des navigateurs web disposent d'un moteur JavaScript6 pour l'interpréter.
+
+JavaScript est aussi employé pour les serveurs Web7 avec l'utilisation (par exemple) de Node.js8 ou de Deno9.
+
+JavaScript a été créé en 1995 par Brendan Eich et intégré au navigateur web Netscape Navigator 2.0. L'implémentation concurrente de JavaScript par Microsoft dans Internet Explorer jusqu'à sa version 9 se nommait JScript, tandis que celle d'Adobe Systems se nommait ActionScript. JavaScript a été standardisé sous le nom d'ECMAScript en juin 1997 par Ecma International dans le standard ECMA-262. La version en vigueur de ce standard depuis juin 2022 est la 13e édition.
+
+C'est un langage orienté objet à prototype : les bases du langage et ses principales interfaces sont fournies par des objets. Cependant, à la différence d'un langage orienté objets à classes, les objets de base ne sont pas des instances de classes. En outre, les fonctions sont des objets de première classe. Le langage supporte le paradigme objet, impératif et fonctionnel.
+
+JavaScript est le langage possédant le plus large écosystème grâce à son gestionnaire de dépendances npm, avec plus de deux millions de paquets en septembre 202210."
+        ],
+
+
+    ]
+];
---
+++ public/index.php
@@ -0,0 +1,2 @@
+<?php
+// Front Controller
