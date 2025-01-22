76c888a50c4b4be002fbe66727b5d83ed479e8bb
vahag1991 <smbatyanvahagn1991@gmail.com>
2025-01-22 13:29:53 +0100

    index

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 # public\index.php
 
 // Front Controller
-<<<<<<< HEAD
 
 // chargement des données (les dépendances)
 include "../datas/datas.php";
@@ -16,8 +15,3 @@ include "../controller/router.php";
 
 // Appel de la vue
 include "../view/MyView.php";
-=======
-include "../datas/datas.php";
-var_dump($datas) ; 
-include "..controller/router.php";
->>>>>>> fe4febb61e3988a3e4b157bebe7d2ebc2a3ceb8e
